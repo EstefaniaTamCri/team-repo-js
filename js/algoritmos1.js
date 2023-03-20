64394cd867372bf73cb0ef0081c6fb2d1cfcd301
Cayetano97 <lopezcayetano315@gmail.com>
2023-03-20 16:03:17 +0100

    última actualización

--- js/algoritmos1.js
+++ js/algoritmos1.js
@@ -1,11 +1,15 @@
 // A1 E7
 
 function A1E7() {
+
+  // Varibles de euros y tasas de cambio
   let euros = prompt("Introduzca Euros a convertir");
   let cambiodolar = 1.07;
   let cambioyen = 141.04;
   let cambiolibra = 0.88;
   let cambiofranco = 0.99;
+
+// Si el valor introducido en el promt es un número hacer el cambio a todas las monedas.
 
   switch (!isNaN(euros)) {
     case true:
@@ -15,6 +19,7 @@ function A1E7() {
       tasafranco = euros * cambiofranco;
       break;
 
+// Si no es un valor númerico comunicar el aviso.
     default:
       console.log("Introduza un valor de euros para el cambio");
       break;
@@ -26,14 +31,19 @@ function A1E7() {
 }
 //  A1 E8
 function A1E8() {
+
+  // Pedimos el número 1 y número 2 al usuario. Despues la operación a realizar.
   let num1 = parseInt(prompt("Introduzca número 1"));
   let num2 = parseInt(prompt("Introduzca número 2"));
   let operacion = prompt("Elige la operación (suma, resta, multi, divide");
 
+ // Si el valor introducido en el número 1 u 2 no es númerico, muestra aviso. 
   if (!isNaN(num1) == false) {
     console.log("Introduzca un valor correcto");
   } else if (!isNaN(num2) == false) {
     console.log("Introduzca un valor correcto");
+
+  // Si el valor introducido es numérico procede a hacer la operación.
   } else {
     switch (operacion) {
       case "suma":
@@ -46,6 +56,9 @@ function A1E8() {
       case "multi":
         console.log("El resultado de la multiplicación es: ", num1 * num2);
         break;
+
+    // En el caso de la división, no se puede dividir un número menor (num1) entre uno mayor (num2)
+    // Muestra aviso si eso ocurre.
       case "divide":
         if (num2 > num1) {
           console.log("No puedes dividir un número menor entre uno mayor");
@@ -54,7 +67,7 @@ function A1E8() {
         }
 
         break;
-
+// Si introduce otro valor distinto a una operación muestra aviso.
       default:
         console.log("Eliga una operación correcta");
         break;
@@ -62,4 +75,3 @@ function A1E8() {
   }
 }
 
-// console.log(A1E7());
--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -35,11 +35,15 @@ function A2E4() {
 let temp = [22, 23, 20, 22, 21, 24, 24]
 let suma = 0
 
+// Recorrer array desde el inico hasta el final
+// Hacer la suma de las temperaturas y dividirlas entre el número de valores en el array para la media
 for (let start = 0; start < temp.length; start++) {
   suma += temp[start];
   media = suma / temp.length;
 }
-console.log("La temperatura media de Málaga en los ultimos 7 días ha sido de: ",media.toFixed(2),"º");
+// Mostrar en consola la media con dos digitos decimales
+let resultado = ("La temperatura media de Málaga en los ultimos 7 días ha sido de: ",media.toFixed(2),"º");
+return resultado
 }
 
 
